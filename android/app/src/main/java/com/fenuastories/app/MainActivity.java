e537fa44ad6ef19b4650c25080285d5c0ed33334
cocogreencar <cocogreencar@gmail.com>
2026-09-17 08:35:14 +0200

    Add Capacitor for Android setup

---
+++ android/app/src/main/java/com/fenuastories/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fenuastories.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.fenuastories.app",
+  appName: "Fenua Stories",
+  webDir: "dist",
+};
+
+export default config;
